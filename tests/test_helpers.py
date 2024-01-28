6c096b0a800199a648357c689e0cb86d4ad55a4f
Are Schjetne <sixcare.as@gmail.com>
2024-01-28 08:12:09 +0100

    Init tests

---
+++ tests/test_helpers.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+
+from turplanlegger.main import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+def test_read_test():
+    response = client.get("/test")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
