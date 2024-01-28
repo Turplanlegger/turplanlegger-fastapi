bda12dd876035c529c940f2b5cae18062049c99a
Are Schjetne <sixcare.as@gmail.com>
2024-01-28 08:13:37 +0100

    Lint using ruff

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,15 +1,16 @@
 from fastapi.testclient import TestClient
-
 from turplanlegger.main import app
 
 client = TestClient(app)
 
+
 def test_read_main():
-    response = client.get("/")
+    response = client.get('/')
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {'message': 'Hello World'}
+
 
 def test_read_test():
-    response = client.get("/test")
+    response = client.get('/test')
     assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
+    assert response.json() == {'status': 'ok'}
