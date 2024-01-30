3ae08081fb3aa0238068193886ce2e3e0215aaae
Are Schjetne <sixcare.as@gmail.com>
2024-01-30 19:20:56 +0100

    Add prefix to urls

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -5,12 +5,12 @@ client = TestClient(app)
 
 
 def test_read_main():
-    response = client.get('/')
+    response = client.get('/v1')
     assert response.status_code == 200
     assert response.json() == {'message': 'Hello World'}
 
 
 def test_read_test():
-    response = client.get('/test')
+    response = client.get('/v1/test')
     assert response.status_code == 200
     assert response.json() == {'status': 'ok'}
