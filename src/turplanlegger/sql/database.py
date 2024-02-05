cd845c739d186f255525eecf3a01cbf309d7d218
Are Schjetne <sixcare.as@gmail.com>
2024-02-05 21:59:44 +0100

    :fire: Remove unused init

--- src/turplanlegger/sql/database.py
+++ src/turplanlegger/sql/database.py
@@ -1,19 +1,14 @@
 from typing import Optional
-from sqlmodel import Field, Session, SQLModel, create_engine
-from schemas import *
+from sqlmodel import SQLModel, create_engine
+from turplanlegger.sql import models
 
 class Database:
-  def __init__(self,app=None):
-    self.app = None
-    if app:
-      self.init_db
-
-  def connect(self, app):
-    engine = create_engine(app.config.get('DATABASE_URI'))
+  def connect(self):
+    engine = create_engine(
+      'postgresql+psycopg://turadm:passord@localhost:5432/turplanlegger?connect_timeout=10&application_name=turplanlegger-api',
+      echo=True
+    )
     SQLModel.metadata.create_all(engine)
-    
+    return engine
 
-  def _log(self, func_name, query, vars):
-    self.logger.debug('\n{stars} {func_name} {stars}\n{query}'.format(
-      stars='*' * 20,func_name=func_name, query=self.cur.mogrify(query, vars)))
-
+engine = Database().connect()
