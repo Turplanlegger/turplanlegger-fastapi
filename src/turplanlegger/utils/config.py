30bb3bdae5cbd8e5076cde28825a2d117e9c853c
Petter <43450467+peseb@users.noreply.github.com>
2024-02-13 20:43:21 +0100

    Update src/turplanlegger/utils/config.py
    Co-authored-by: Are Schjetne <sixcare.as@gmail.com>

--- src/turplanlegger/utils/config.py
+++ src/turplanlegger/utils/config.py
@@ -21,7 +21,7 @@ class Settings(BaseSettings):
     DATABASE_URI: str = Field(min_length=1)
     DATABASE_MAX_RETRIES: int = Field(5)
 
-    # Logging
+    # CORS
     CORS_ORIGINS: list = Field(['http://localhost:3000'])
 
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', env_prefix='TP_')
