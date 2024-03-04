e4a59118d92026752d573f613424eee38ebdd82b
Are Schjetne <sixcare.as@gmail.com>
2024-03-04 21:26:19 +0100

    :fire: Remove database class

--- src/turplanlegger/sql/database.py
+++ src/turplanlegger/sql/database.py
@@ -1,19 +1,15 @@
 from typing import Optional
 from sqlmodel import SQLModel, Session, create_engine
-from turplanlegger.sql import models
 
-class Database:
-  def __init__(self):
-    self.uri = 'postgresql+psycopg://turadm:passord@localhost:5432/turplanlegger?connect_timeout=10&application_name=turplanlegger-fastapi'
-    self.debug = True
 
-  def connect(self):
-    engine = create_engine(
-      self.uri,
-      echo=self.debug
-    )
+DATABASE_URI = 'postgresql+psycopg://turadm:passord@localhost:5432/turplanlegger?connect_timeout=10&application_name=turplanlegger-fastapi'
+DEBUG = True
+
+engine = create_engine(DATABASE_URI, echo=DEBUG)
+
+def init_db():
     SQLModel.metadata.create_all(engine)
-    return engine
-  
-  def session(self, engine):
-    return Session(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
