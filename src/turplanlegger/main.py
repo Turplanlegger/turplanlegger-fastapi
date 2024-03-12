e493719329252d00ad636a5d779da7b2bc40e65d
Petter Bjørkås <peseb@icloud.com>
2024-03-12 20:09:22 +0100

    Setup CORS

--- src/turplanlegger/main.py
+++ src/turplanlegger/main.py
@@ -1,15 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.turplanlegger.utils.config import get_settings
 
 from .__about__ import __version__
 from .routers import helpers, users
 from .sql.database import init_db
 
+config = get_settings()
 
 def create_app() -> FastAPI:
     app_ = FastAPI(
         title='Turplanlegger',
         description='Turplanlegger API',
         version=__version__,
+    )
+
+    app_.add_middleware(
+        CORSMiddleware,
+        allow_origins=config.CORS_ORIGINS,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     app_.include_router(helpers.router, prefix='/v1')
