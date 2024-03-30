6ad203abc3de25703aa173a733a79ede4d06ae1c
Are Schjetne <sixcare.as@gmail.com>
2024-03-30 17:57:52 +0100

    ♻️ Refactor query user to use new User models

--- src/turplanlegger/routers/users.py
+++ src/turplanlegger/routers/users.py
@@ -39,8 +39,8 @@ def get_user(user_id: str, session: Session = Depends(get_session)):
     return db_user
 
 
-@router.get('/query/', description='Query user by email', response_model=User)
-def query_user(email: str, session: Session = Depends(get_session)):
+@router.get('/query/', description='Query user by email')
+def query_user(email: str, session: Session = Depends(get_session)) -> UserRead:
     db_user = crud.get_user_by_email(session, email=email)
     if db_user is None:
         raise HTTPException(status_code=404, detail='User not found')
