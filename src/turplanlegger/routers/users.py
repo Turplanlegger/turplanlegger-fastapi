2c2d866db5ebfc0d07bb1cb97005c0b3ef2e3108
Are Schjetne <sixcare.as@gmail.com>
2024-04-03 20:23:52 +0200

    🚨 Fix MyPy errors https://fastapi.tiangolo.com/tutorial/response-model/#response_model-parameter

--- src/turplanlegger/routers/users.py
+++ src/turplanlegger/routers/users.py
@@ -15,16 +15,16 @@ router = APIRouter(
 )
 
 
-@router.get('/', description='Get all users')
-def all_users(session: Session = Depends(get_session)) -> list[UserRead]:
+@router.get('/', description='Get all users', response_model=list[UserRead])
+def all_users(session: Session = Depends(get_session)):
     db_users = crud.get_all_users(session)
     if not db_users:
         raise HTTPException(status_code=404, detail='No users found')
     return db_users
 
 
-@router.post('/', description='Create a new user')
-def create_user(user: UserCreate, session: Session = Depends(get_session)) -> UserRead:
+@router.post('/', description='Create a new user', response_model=UserRead)
+def create_user(user: UserCreate, session: Session = Depends(get_session)):
     db_user = crud.get_user_by_email(session, email=user.email)
     if db_user is not None:
         raise HTTPException(status_code=400, detail='User exists')
@@ -33,16 +33,16 @@ def create_user(user: UserCreate, session: Session = Depends(get_session)) -> Us
     return db_user
 
 
-@router.get('/{user_id}', description='Get user by id')
-def get_user(user_id: UUID, session: Session = Depends(get_session)) -> UserRead:
+@router.get('/{user_id}', description='Get user by id', response_model=UserRead)
+def get_user(user_id: UUID, session: Session = Depends(get_session)):
     db_user = crud.get_user(session, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail='User not found')
     return db_user
 
 
-@router.get('/query/', description='Query user by email')
-def query_user(email: str, session: Session = Depends(get_session)) -> UserRead:
+@router.get('/query/', description='Query user by email', response_model=UserRead)
+def query_user(email: str, session: Session = Depends(get_session)):
     db_user = crud.get_user_by_email(session, email=email)
     if db_user is None:
         raise HTTPException(status_code=404, detail='User not found')
@@ -60,8 +60,8 @@ def delete_user(user_id: UUID, session: Session = Depends(get_session)) -> Respo
     return Response(status_code=HTTPStatus.NO_CONTENT.value)
 
 
-@router.put('/{user_id}', description='Update user by id')
-def update_user(user_id: UUID, user_updates: UserUpdate, session: Session = Depends(get_session)) -> UserRead:
+@router.put('/{user_id}', description='Update user by id', response_model=UserRead)
+def update_user(user_id: UUID, user_updates: UserUpdate, session: Session = Depends(get_session)):
     db_user = crud.get_user(session, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail='User not found')
--- src/turplanlegger/sql/crud.py
+++ src/turplanlegger/sql/crud.py
@@ -1,9 +1,10 @@
+from typing import Sequence
 from uuid import UUID
 
 from sqlmodel import Session, delete, select
 
 from .database import engine
-from .models import User, UserCreate, UserRead, UserUpdate
+from .models import User, UserCreate, UserUpdate
 
 
 def delete_all_users() -> None:
@@ -13,22 +14,22 @@ def delete_all_users() -> None:
         session.commit()
 
 
-def get_all_users(db: Session) -> list[UserRead]:
+def get_all_users(db: Session) -> Sequence[User]:
     statement = select(User)
     return db.exec(statement).all()
 
 
-def get_user(db: Session, user_id: UUID) -> UserRead:
+def get_user(db: Session, user_id: UUID) -> User | None:
     statement = select(User).where(User.id == user_id)
     return db.exec(statement).one_or_none()
 
 
-def get_user_by_email(db: Session, email: str) -> UserRead:
+def get_user_by_email(db: Session, email: str) -> User | None:
     statement = select(User).where(User.email == email)
     return db.exec(statement).one_or_none()
 
 
-def create_user(db: Session, user: UserCreate) -> UserRead:
+def create_user(db: Session, user: UserCreate) -> User | None:
     db_user = User.model_validate(user)
     db.add(db_user)
     db.commit()
@@ -41,7 +42,7 @@ def delete_user(db: Session, user: User) -> None:
     db.commit()
 
 
-def update_user(db: Session, db_user: User, user_updates: UserUpdate) -> UserRead:
+def update_user(db: Session, db_user: User, user_updates: UserUpdate) -> User | None:
     updated = False
 
     for attr_name, attr_value in user_updates.__dict__.items():
